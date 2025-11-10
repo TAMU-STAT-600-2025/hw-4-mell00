3deb826d23ef295b7369323fd64f2184b2677e97
Madison Ell <mellmell2550@gmail.com>
2025-11-09 22:48:41 -0600

    Update riboflavin dataset median timing

--- RiboflavinDataAnalysis.R
+++ RiboflavinDataAnalysis.R
@@ -41,7 +41,7 @@ print(mb)
 median_seconds <- median(mb$time) / 1e9  # convert nanoseconds into seconds
 cat(sprintf("Median time over 10 runs: %.3f seconds\n", median_seconds))
 
-# 0.088 sec
+# 0.096 sec
 
 # Use cvLASSO function on the riboflavin data with 30 tuning parameters (just 30 to make it faster)
 set.seed(42)  # seed for reproducibility
